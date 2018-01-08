e71442c863c802444f59971e117d6ca6a77f91e5
guanshanzhi <124150224@qq.com>
2018-01-08 17:32:47 +0800

    1111

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -18,6 +18,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"你好");
+    
+    NSLog(@"你好");
+    NSLog(@"你好");
+    
+    NSLog(@"你好");
 }
 
 
