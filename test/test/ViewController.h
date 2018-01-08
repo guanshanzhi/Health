4b5680179f880ae10b4147c0a573e5063e6368cb
guanshanzhi <124150224@qq.com>
2018-01-08 17:18:33 +0800

    111

---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by luyeyige on 2018/1/8.
+//  Copyright © 2018年 guanshanzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by luyeyige on 2018/1/8.
+//  Copyright © 2018年 guanshanzhi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
